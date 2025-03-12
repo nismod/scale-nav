4cf2c22edcd5c6a328e4eb876a9771e24674c1e3
ischlo <ivann.schlosser.19@ucl.ac.uk>
2025-03-12 16:29:32 +0000

    tests update

--- src/scalenav/data.py
+++ src/scalenav/data.py
@@ -1,108 +1,124 @@
 from os.path import exists
-from numpy import array,meshgrid,arange,log
+from numpy import array, meshgrid, arange, log
 from rasterio.transform import xy
 from rasterio import open
+
 # from rasterio.warp import re
-from geopandas import GeoSeries, GeoDataFrame,read_file
-from shapely.geometry import Point,box,Polygon
+from geopandas import GeoSeries, GeoDataFrame, read_file
+from shapely.geometry import Point, box, Polygon
+
 # import shapely as shp
-from pandas import Series,concat, DataFrame
-from math import pi,cos
+from pandas import Series, concat, DataFrame
+from math import pi, cos
 import h3
 from re import search
-from scalenav.utils import earth_radius_meters,A,alpha
+from scalenav.utils import earth_radius_meters, A, alpha
+
 # from overloading import overload
 from functools import singledispatch
 
+
 @singledispatch
-def df_project_on_grid(data : GeoDataFrame, res : int = 11) -> GeoDataFrame:
-    """ Project a geopandas.GeoDataFrame containing points on the H3 grid at a given resolution. 
+def df_project_on_grid(data: GeoDataFrame, res: int = 11) -> GeoDataFrame:
+    """Project a geopandas.GeoDataFrame containing points on the H3 grid at a given resolution.
 
     Parameters
     -----------
     data : a geopandas.GeoDataFrame with a geometry column containing points.
     res : a integer value of H3 resolution.
-    
+
     Return
     ----------
-    The original data frame with a new column called h3_id ccontaining the H3 code for each geometry. 
+    The original data frame with a new column called h3_id ccontaining the H3 code for each geometry.
 
     """
 
-    data["h3_id"] = data["geometry"].apply(lambda point: h3.latlng_to_cell(lng=point.x,lat=point.y,res=res))
+    data["h3_id"] = data["geometry"].apply(
+        lambda point: h3.latlng_to_cell(lng=point.x, lat=point.y, res=res)
+    )
     return data
 
 
 @df_project_on_grid.register
-def _(data : DataFrame, res : int = 11) -> DataFrame:
-    """ Project a pandas.DataFrame containing points coordinates on the H3 grid at a given resolution. 
+def _(data: DataFrame, res: int = 11) -> DataFrame:
+    """Project a pandas.DataFrame containing points coordinates on the H3 grid at a given resolution.
 
     Parameters
     -----------
     data : a pandas.DataFrame with x,y column containing containing coordinates.
     res : a integer value of H3 resolution.
-    
+
     Return
     ----------
-    The original data frame with a new column called h3_id ccontaining the H3 code for each geometry. 
+    The original data frame with a new column called h3_id ccontaining the H3 code for each geometry.
     """
 
-    data["h3_id"] = data[["x","y"]].apply(lambda point: h3.latlng_to_cell(lng=point.loc["x"],lat=point.loc["y"],res=res),axis=1)
+    data["h3_id"] = data[["lon", "lat"]].apply(
+        lambda point: h3.latlng_to_cell(
+            lng=point.loc["lon"], lat=point.loc["lat"], res=res
+        ),
+        axis=1,
+    )
     return data
 
-def pt_project_on_grid(lat : float,lon : float, res : int = 11) -> DataFrame:
-    """ Get H3 index of lat,lon coordinates for a resolution. Simple wrapper around the H3.latlng_to_cell function.
+
+def pt_project_on_grid(lat: float, lon: float, res: int = 11) -> DataFrame:
+    """Get H3 index of lat,lon coordinates for a resolution. Simple wrapper around the H3.latlng_to_cell function."""
+    return h3.latlng_to_cell(lng=lon, lat=lat, res=res)
+
+
+def square_poly(
+    lat: float, lon: float, distance: int = 10_000, ref: str = "arc"
+) -> Polygon:
+    """Make a square box with side size given in the distance parameter centered on the (lon,lat) point.
+    The distance is expected to be in meters. The coordinates in degrees according to WGS:84.
     """
-    return h3.latlng_to_cell(lng=lon,lat=lat,res=res)
+    half_distance = distance / 2
 
+    lat_rad = pi * lat / 180
 
-def square_poly(lat : float, lon : float, distance : int = 10_000, ref : str = "arc") -> Polygon:
-    """ Make a square box with side size given in the distance parameter centered on the (lon,lat) point. 
-    The distance is expected to be in meters. The coordinates in degrees according to WGS:84. 
-    """
-    half_distance = distance/2
-
-    lat_rad = pi*lat/180
-
-    dphi = half_distance/cos(lat_rad)/earth_radius_meters/pi*180
+    dphi = half_distance / cos(lat_rad) / earth_radius_meters / pi * 180
 
     if ref == "m":
         # print("Using angles for meter grid.")
-        dtheta = half_distance/earth_radius_meters/pi*180
+        dtheta = half_distance / earth_radius_meters / pi * 180
     elif ref == "arc":
         # print("Using angles for arc grid.")
-        dtheta = half_distance/cos(lat_rad)/earth_radius_meters/pi*180
-    else :
+        dtheta = half_distance / cos(lat_rad) / earth_radius_meters / pi * 180
+    else:
         raise Warning("Provide a valid ref parameter.")
-    
 
-    xlim = (lon-dphi,lon+dphi)
-    ylim = (lat-dtheta,lat+dtheta)
+    xlim = (lon - dphi, lon + dphi)
+    ylim = (lat - dtheta, lat + dtheta)
 
-    res = box(minx=xlim[0],maxx=xlim[1],miny=ylim[0],maxy=ylim[1])
+    res = box(minx=xlim[0], maxx=xlim[1], miny=ylim[0], maxy=ylim[1])
 
-    return res 
+    return res
 
-def rast_to_h3_map(x : float = 0.0, y : float = 51.51, ref : str = "m", dist : float = 0):
+
+def rast_to_h3_map(x: float = 0.0, y: float = 51.51, ref: str = "m", dist: float = 0):
     """Allows adding a custom distance value for unusual grid shapes and specify if the raster cells are in projected or arc sizes."""
 
     grid_params = []
     res_params = []
 
-    if dist > 0 : 
+    if dist > 0:
 
-        grid_params = [10,100,1000,5000,10000,dist]
+        grid_params = [10, 100, 1000, 5000, 10000, dist]
         grid_params.sort()
 
-        res_params = [round(A-alpha*log(size)) for size in grid_params]
+        res_params = [round(A - alpha * log(size)) for size in grid_params]
 
     else:
-        grid_params = [10,100,1000,5000,10000]
-        res_params = [14,12,11,10,8]
+        grid_params = [10, 100, 1000, 5000, 10000]
+        res_params = [14, 12, 11, 10, 8]
 
     # res_params
 
-    rast_to_h3 = { str(size): {"h3_res" : res, "nn" : [] } for (size,res) in zip(grid_params,res_params)}
+    rast_to_h3 = {
+        str(size): {"h3_res": res, "nn": []}
+        for (size, res) in zip(grid_params, res_params)
+    }
 
     # grid_params = [10,100,1000,5000,10000]
     # res_params = [13,12,11,10,8]
@@ -129,22 +145,27 @@ def rast_to_h3_map(x : float = 0.0, y : float = 51.51, ref : str = "m", dist : f
         # get the right h3 resolution for the grid param.
         res_h3 = rast_to_h3[str(grid)]["h3_res"]
 
-        square = square_poly(lat=y,lon=x,distance=grid,ref=ref)
+        square = square_poly(lat=y, lon=x, distance=grid, ref=ref)
 
-        ref_cell = h3.latlng_to_cell(lat=y,lng=x,res=res_h3)
-        ref_cell_ij = h3.cell_to_local_ij(origin=ref_cell,h=ref_cell)
+        ref_cell = h3.latlng_to_cell(lat=y, lng=x, res=res_h3)
+        ref_cell_ij = h3.cell_to_local_ij(origin=ref_cell, h=ref_cell)
 
-        cells = h3.geo_to_cells(square,res=res_h3)
+        cells = h3.geo_to_cells(square, res=res_h3)
 
-        cells_ij = [h3.cell_to_local_ij(origin=ref_cell,h=cell) for cell in cells]
+        cells_ij = [h3.cell_to_local_ij(origin=ref_cell, h=cell) for cell in cells]
 
-        rast_to_h3[str(grid)]["nn"] = [(cell_i-ref_cell_ij[0],cell_j-ref_cell_ij[1]) for (cell_i,cell_j) in cells_ij]
+        rast_to_h3[str(grid)]["nn"] = [
+            (cell_i - ref_cell_ij[0], cell_j - ref_cell_ij[1])
+            for (cell_i, cell_j) in cells_ij
+        ]
 
     return rast_to_h3
 
-rast_to_h3 = rast_to_h3_map(x = 0, y = 0)
 
-def centre_cell_to_square(h3_cell : str, neighbs : list[tuple[int]]) -> list[str]:
+rast_to_h3 = rast_to_h3_map(x=0, y=0)
+
+
+def centre_cell_to_square(h3_cell: str, neighbs: list[tuple[int]]) -> list[str]:
     """If a centroid h3 index is known, return the square cover for a cell and grid_param"""
 
     sql = """SQL
@@ -152,13 +173,16 @@ def centre_cell_to_square(h3_cell : str, neighbs : list[tuple[int]]) -> list[str
     h3_local_ij_to_cell(h3_id,neighbs[:][0],neighbs[:][1])
     """
 
-    ref_cell_ij = h3.cell_to_local_ij(origin=h3_cell,h=h3_cell)
+    ref_cell_ij = h3.cell_to_local_ij(origin=h3_cell, h=h3_cell)
 
-    return [h3.local_ij_to_cell(origin=h3_cell,i=cell_i+ref_cell_ij[0],j=cell_j+ref_cell_ij[1]) for (cell_i,cell_j) in neighbs]
+    return [
+        h3.local_ij_to_cell(
+            origin=h3_cell, i=cell_i + ref_cell_ij[0], j=cell_j + ref_cell_ij[1]
+        )
+        for (cell_i, cell_j) in neighbs
+    ]
 
-def layer_constrain(layer : [DataFrame,GeoDataFrame], constraint : [DataFrame,Series]):
-    """ Apply a constraint to a layer. Remove the constrained cells from the layer and rescale the values.
-    """
+
+def layer_constrain(layer: [DataFrame, GeoDataFrame], constraint: [DataFrame, Series]):
+    """Apply a constraint to a layer. Remove the constrained cells from the layer and rescale the values."""
     return layer
-
-
--- tests/test_scale-nav.py
+++ tests/test_scale-nav.py
@@ -17,42 +17,44 @@ import ibis as ib
 size = 10
 h3_res = 10
 
-x = np.random.uniform(low=-180,high=180,size=size).tolist()
-y = np.random.uniform(low=-90,high=90,size=size).tolist()
-band_var = np.random.exponential(scale=5,size=size).tolist()
+x = np.random.uniform(low=-180, high=180, size=size).tolist()
+y = np.random.uniform(low=-90, high=90, size=size).tolist()
+band_var = np.random.exponential(scale=5, size=size).tolist()
 
 coords_geom = gpd.GeoDataFrame(
-    data = {"band_var" : band_var},
-    geometry=gpd.GeoSeries.from_xy(
-                                x=x,
-                                y=y,
-                                crs="epsg:4326",
-                                name="geom"),
+    data={"band_var": band_var},
+    geometry=gpd.GeoSeries.from_xy(x=x, y=y, crs="epsg:4326", name="geom"),
 )
 
-coords_xy = pd.DataFrame(data={"lon" : x,
-                            "lat" : y,
-                            "band_var" : band_var,},
+coords_xy = pd.DataFrame(
+    data={
+        "lon": x,
+        "lat": y,
+        "band_var": band_var,
+    },
 )
 
 
 def test_project_on_grid():
 
-    bench = [h3.latlng_to_cell(lng=x,lat=y,res=h3_res) for (x,y) in zip(coords_xy["lon"],coords_xy["lat"])]
-    res = sd.df_project_on_grid(coords_xy,res = h3_res)["h3_id"].to_list()
+    bench = [
+        h3.latlng_to_cell(lng=x, lat=y, res=h3_res)
+        for (x, y) in zip(coords_xy["lon"], coords_xy["lat"])
+    ]
+    res = sd.df_project_on_grid(coords_xy, res=h3_res)["h3_id"].to_list()
 
-    assert res==bench
+    assert res == bench
 
 
 def test_change_scale_xy():
-    
-    res_h3 = sd.df_project_on_grid(coords_xy,res = h3_res)
 
-    bench = list(iter.chain.from_iterable([[x/7]*7 for x in band_var]))
+    res_h3 = sd.df_project_on_grid(coords_xy, res=h3_res)
 
-    res = sn.change_res(res_h3,1)
+    bench = list(iter.chain.from_iterable([[x / 7] * 7 for x in band_var]))
 
-    assert res["band_var"].to_list()==bench, ""
+    res = sn.change_res(res_h3, 1)
+
+    assert res["band_var"].to_list() == bench, ""
 
 
 def test_scale_change_down():
@@ -60,29 +62,35 @@ def test_scale_change_down():
     conn = snoo.sn_connect()
 
     test_start_res = 7
-    test_levels = [1,3]
+    test_levels = [1, 3]
 
-    h3_id = [h3.latlng_to_cell(y,x,res=test_start_res) for (x,y) in zip(x,y)]
+    h3_id = [h3.latlng_to_cell(y, x, res=test_start_res) for (x, y) in zip(x, y)]
 
-    resc_test = pd.DataFrame({"h3_id" : h3_id})
+    resc_test = pd.DataFrame({"h3_id": h3_id})
     resc_test["vals_var"] = 7
-    
-    resc_test_back = conn.create_table("resc_test",resc_test)
+
+    resc_test_back = conn.create_table("resc_test", resc_test)
 
     for lev in test_levels:
 
-        res_pd = sn.change_res(resc_test,lev)
+        res_pd = sn.change_res(resc_test, lev)
         # generating the data set with the new
-        res_ib = snoo.sn_change_res(resc_test_back,lev)
+        res_ib = snoo.sn_change_res(resc_test_back, lev)
 
         res_df_down = res_ib.execute()
 
-        assert np.all(res_df_down.vals_var==res_pd.vals_var), ""
-        assert res_df_down.shape[0]==res_pd.shape[0],  ""
-        assert len(np.intersect1d(res_df_down.h3_id,res_pd.h3_id))==res_df_down.shape[0], ""
+        assert np.all(res_df_down.vals_var == res_pd.vals_var), ""
+        assert res_df_down.shape[0] == res_pd.shape[0], ""
+        assert (
+            len(np.intersect1d(res_df_down.h3_id, res_pd.h3_id)) == res_df_down.shape[0]
+        ), ""
 
-        res_df_up = snoo.sn_change_res(res_ib,levels=-lev).execute()
+        res_df_up = snoo.sn_change_res(res_ib, levels=-lev).execute()
 
-        assert np.all(np.float32(res_df_up.vals_var)==np.float32(resc_test.vals_var)), ""
-        assert res_df_up.shape[0]==resc_test.shape[0], ""
-        assert len(np.intersect1d(res_df_up.h3_id,resc_test.h3_id))==res_df_up.shape[0], ""
+        assert np.all(
+            np.float32(res_df_up.vals_var) == np.float32(resc_test.vals_var)
+        ), ""
+        assert res_df_up.shape[0] == resc_test.shape[0], ""
+        assert (
+            len(np.intersect1d(res_df_up.h3_id, resc_test.h3_id)) == res_df_up.shape[0]
+        ), ""
